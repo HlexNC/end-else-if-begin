4aa618136cf122804fd94bb2cb7ae53960bd1393
HlexNC <pixelfurno@gmail.com>
2021-03-01 13:21:42 +0300

    solved 1_1_3

--- 1_1_3.pas
+++ 1_1_3.pas
@@ -1,5 +1,26 @@
-k:= n; b := 1;
-while k <> n do begin
-    k := k + 1;
-    b := b * a;
-end;
+program 1_1_3;
+
+var
+a, n, k, b: integer;
+
+begin
+    k:= 0; b := 1;
+    {b = a в степени k}
+    while k <> n do begin
+        k := k + 1;
+        b := b * a;
+    end;
+end.
+
+{
+program main;
+var a, n, k, b:integer;
+begin
+    k:=0; b:=1;
+    readln(a, n);
+    while k <> n do begin
+        k := k + 1;
+        b := b * a
+    end;
+    writeln(b)
+end.}
